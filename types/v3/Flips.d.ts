35f1d0a950d2b5cca5d21f37cc1e14316ca1999e
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-24 19:41:10 +0800

    build

---
+++ types/v3/Flips.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+interface FlipsProps {
+    wrap?: string;
+    wrapClass?: string;
+    name?: string;
+    children?: React.ReactNode;
+}
+declare const _default: React.NamedExoticComponent<FlipsProps>;
+export default _default;
